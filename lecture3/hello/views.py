f9d49384f8255ec402d5653fe12c6f74cadbcab4
eylul-sahin <sahin.eylul@hotmail.com>
2023-10-03 18:59:05 +0300

    lecture 3 (create a todo list using forms)

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
